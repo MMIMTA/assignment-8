429771b10a9a4ebf7b07339e3dfae1293bf6a41b
Marjia <marjiamimtaa@gmail.com>
2023-04-10 08:41:01 -0600

    heading created

--- src/Components/Exercise/Exercise.js
+++ src/Components/Exercise/Exercise.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import './Exercise.css';
 
 const Exercise = (props) => {
     const{id,name,picture,time}=props.exercise;
 
     return (
         <div>
+          
+        <div className='image'>
             <img src={picture} alt="" />
+        </div>
         </div>
     );
 };
--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Exercise from '../Exercise/Exercise';
+import Cart from '../Cart/Cart';
+import './MainPage.css';
+import exercise from '../../images/exercise.png'
 
 const MainPage = () => {
    const[exercises, setExercises]=useState([]);
@@ -12,14 +15,25 @@ const MainPage = () => {
    },[]);
 
     return (
-        <div>
+      <div>
+        
+        <div className='Exercise-site'>  
           <div>
+           
+          <p className='Heading'><img className='logo' src={exercise} alt="" />Strength and Fitness Palace</p>
+          <p className='sec-heading'>Select todays exercise</p>
+          <div className='container'>
             {
              exercises.map(exercise=><Exercise
              key={exercise.id}
              exercise={exercise}></Exercise>)
             }
           </div>
+          </div>
+          <div>
+            <Cart></Cart>
+          </div>
+        </div>
         </div>
     );
 };
